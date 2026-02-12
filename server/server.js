8b74c2f49ee76f136aa838e8a9df3c9edec9c71d
MarkusJakobR <mcrecaplaza@up.edu.ph>
2026-02-12 19:39:47 +0800

    feat: connected project to supabase

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const supabase = require("./config/supabase");
+
 const app = express();
 
 // Allow requests from your frontend
@@ -15,5 +17,13 @@ app.use(
 app.use(express.json());
 
 app.get("/", (req, res) => res.send("API running"));
+
+// Test DB route
+app.get("/test-db", async (req, res) => {
+  const { data, error } = await supabase.from("decks").select("*");
+
+  if (error) return res.status(500).json(error);
+  res.json(data);
+});
 
 app.listen(4000, () => console.log("Server on port 4000"));
